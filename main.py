33225ede3ca5e177df1ef0c26434321b4c0f1e47
RAMIhamza <32485004+RAMIhamza@users.noreply.github.com>
2020-03-10 12:23:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,107 @@
+import numpy as np
+import pandas as pd
+from layers import conv,maxpooling,fully_connected
 
+def cross_entropy_loss(output,true_label):
+    return -np.log(output[true_label])
+    
+def accuracy(output,true_label):
+    predicted_class=np.argmax(output)
+    if predicted_class==true_label:
+        return 1
+    else :
+        return 0
+      
+conv_ = conv(8)
+pool = maxpooling(2)
+fully_c=fully_connected(13*13*8,10)
+
+def forward(image, label):
+
+  out = conv_.forward(image)
+  out = pool.forward(out)
+  out = fully_c.forward(out)
+
+  # Calculate cross-entropy loss and accuracy.
+  loss = cross_entropy_loss(out,label)
+#   print(out[label])
+  acc = accuracy(out,label)
+
+  return out, loss, acc
+
+def predict(image):
+    
+    out = conv_.forward(image)
+    out = pool.forward(out)
+    out = fully_c.forward(out)
+    return np.argmax(out) 
+  
+def train(im, label):
+  '''
+  Completes a full training step on the given image and label.
+  Returns the cross-entropy loss and accuracy.
+  - image is a 2d numpy array
+  - label is a digit
+  - lr is the learning rate
+  '''
+  # Forward
+  out, loss, acc = forward(im, label)
+  # Backprop
+  gradient = fully_c.backward(im,label)
+  gradient_2 = pool.backward(gradient)
+  gradient_3 = conv_.backward(gradient_2)
+
+  return loss, acc 
+
+print('CNN started!')
+if __name__ == '__main__': 
+    parser = argparse.ArgumentParser("Training settings")
+#     parser.add_argument("--sentence", help="A sentence to parse", nargs='+', type=str)   
+#     parser.add_argument("--print_parsing", help="print the parsinf",default=False ,type=bool)
+    parser.add_argument("--input_train_data", help="A file to parse",default="../input/digit-recognizer/train.csv", type=str)
+    parser.add_argument("--input_test_data", help="A file to parse",default="../input/digit-recognizer/test.csv", type=str)
+#     parser.add_argument("--true_file_name", help="A file to parse",default='../Corpus/sequoia_test.tb', type=str)
+#     parser.add_argument("--prediction_file_name", help="A file to parse",default='evaluation_data.parser_output', type=str)
+#     parser.add_argument("--train_pcfg", help="train the parserf",default=False ,type=bool)
+#     parser.add_argument("--split_data", help="split the data into 80/10/10",default=False ,type=bool)
+#     parser.add_argument("--evaluate", help="Evaluation of a parser",default=False ,type=bool)
+    args = parser.parse_args()
+    # Load the data
+    train = pd.read_csv(args.input_train_data)
+    test = pd.read_csv(args.input_test_data)
+    Y_train = train["label"][:10000]
+    Y_dev = train["label"][10000:12500]
+    # Drop 'label' column
+    X_train = train.drop(labels = ["label"],axis = 1) [:10000]
+    X_dev = train.drop(labels = ["label"],axis = 1) [10000:12500]
+    #normalization
+    # Normalize the data
+    X_train = X_train / 255.0
+    X_dev = X_dev / 255.0
+
+    test = test / 255.0 
+    # Reshape image in 3 dimensions (height = 28px, width = 28px , canal = 1)
+    X_train = X_train.values.reshape(-1,28,28,1)
+    X_dev = X_dev.values.reshape(-1,28,28,1)
+
+    test = test.values.reshape(-1,28,28,1)
+    # Train!
+    loss = 0
+    n_epochs=2
+    losses=[]
+    accs=[]
+    num_correct = 0
+    for epoch in range(n_epochs):
+      for i, (im, label) in enumerate(zip(X_train, Y_train)):
+      #   print(label)
+        if i % 100 == 99:
+          print('[Step %d] Past 100 steps: Average Loss %.3f | Accuracy: %d%%' %(i + 1, loss / 100, num_correct))
+          losses.append(loss / 100)
+          accs.append(num_correct)
+          loss = 0
+          num_correct = 0
+
+        l, acc = train(im, label)
+        loss += l
+        num_correct += acc
+ 
